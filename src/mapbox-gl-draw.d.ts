87f86a6ff80c56cb18d1e32366ecb5308d3c93bc
oyz13085 <oyz13085@gmail.com>
2026-05-16 10:44:41 +0800

    fix: add proper IControl and constructor types for MapboxDraw

--- src/mapbox-gl-draw.d.ts
+++ src/mapbox-gl-draw.d.ts
@@ -1,6 +1,12 @@
 declare module '@mapbox/mapbox-gl-draw' {
-  class MapboxDraw {
+  import type mapboxgl from 'mapbox-gl';
+
+  class MapboxDraw implements mapboxgl.IControl {
+    constructor(options?: Record<string, unknown>);
+    onAdd(map: mapboxgl.Map): HTMLElement;
+    onRemove(map: mapboxgl.Map): void;
     [key: string]: any;
   }
+
   export default MapboxDraw;
 }
